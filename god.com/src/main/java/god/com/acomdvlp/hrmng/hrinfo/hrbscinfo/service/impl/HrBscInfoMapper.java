409f3e478ea100363c723bb7193450806d126238
이백행 <dlqorgod@naver.com>
2025-03-26 15:08:27 +0900

    Sequence Id Generation Service

--- god.com/src/main/java/god/com/acomdvlp/hrmng/hrinfo/hrbscinfo/service/impl/HrBscInfoMapper.java
+++ god.com/src/main/java/god/com/acomdvlp/hrmng/hrinfo/hrbscinfo/service/impl/HrBscInfoMapper.java
@@ -2,9 +2,11 @@ package god.com.acomdvlp.hrmng.hrinfo.hrbscinfo.service.impl;
 
 import org.egovframe.rte.psl.dataaccess.mapper.Mapper;
 
+import god.com.acomdvlp.hrmng.hrinfo.hrbscinfo.service.HrBscInfoVO;
+
 @Mapper
 public interface HrBscInfoMapper {
 
-	int insertHrBscInfo();
+	int insertHrBscInfo(HrBscInfoVO hrBscInfoVO);
 
 }
--- god.com/src/test/java/god/com/acomdvlp/hrmng/hrinfo/hrbscinfo/service/impl/HrBscInfoMapperTest.java
+++ god.com/src/test/java/god/com/acomdvlp/hrmng/hrinfo/hrbscinfo/service/impl/HrBscInfoMapperTest.java
@@ -2,6 +2,16 @@ package god.com.acomdvlp.hrmng.hrinfo.hrbscinfo.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+import javax.annotation.Resource;
+
+import org.egovframe.rte.fdl.cmmn.exception.BaseRuntimeException;
+import org.egovframe.rte.fdl.cmmn.exception.FdlException;
+import org.egovframe.rte.fdl.idgnr.EgovIdGnrService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,6 +26,10 @@ class HrBscInfoMapperTest extends EgovAbstractTestJUnit5 {
 	@Autowired
 	private HrBscInfoMapper hrBscInfoMapper;
 
+//	@Autowired
+	@Resource(name = "hrBscInfoIdGnrService")
+	private EgovIdGnrService hrBscInfoIdGnrService;
+
 	/**
 	 * 조회(단건) select[공통표준단어]
 	 */
@@ -26,14 +40,33 @@ class HrBscInfoMapperTest extends EgovAbstractTestJUnit5 {
 
 		// given
 		HrBscInfoVO hrBscInfoVO = new HrBscInfoVO(); // 로그인정책
-		hrBscInfoVO.setEmplyrId("TEST1"); // 업무사용자ID
+//		hrBscInfoVO.setEmplyrId("TEST1"); // 업무사용자ID
+
+//		String now = LocalDateTime.now().toString();
+		// 현재 시간 (밀리초 포함)
+		long millis = System.currentTimeMillis();
+		// 밀리초를 LocalDateTime으로 변환
+		LocalDateTime ldt = Instant.ofEpochMilli(millis).atZone(ZoneId.systemDefault()).toLocalDateTime();
+		// 포맷 정의
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS");
+		// 포맷 적용
+		String now = ldt.format(formatter);
+		hrBscInfoVO.setEmplyrId("AA_" + now); // 업무사용자ID
+
+		String hrBscInfoId;
+		try {
+			hrBscInfoId = hrBscInfoIdGnrService.getNextStringId();
+		} catch (FdlException e) {
+			throw new BaseRuntimeException("FdlException hrBscInfoIdGnrService", e);
+		}
 
 		if (log.isDebugEnabled()) {
+			log.debug("hrBscInfoId={}", hrBscInfoId);
 			log.debug("hrBscInfoVO={}", hrBscInfoVO);
 		}
 
 		// when
-		int result = hrBscInfoMapper.insertHrBscInfo();
+		int result = hrBscInfoMapper.insertHrBscInfo(hrBscInfoVO);
 
 		if (log.isDebugEnabled()) {
 			log.debug("result={}", result);
