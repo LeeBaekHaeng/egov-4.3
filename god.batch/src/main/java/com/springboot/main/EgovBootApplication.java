0d672ad1839b1f025ec8ad0f3c3aefef94c4cb1a
이백행 <dlqorgod@naver.com>
2025-07-22 13:31:33 +0900

    이클립스 > Source > Format

--- god.batch/src/main/java/com/springboot/main/EgovBootApplication.java
+++ god.batch/src/main/java/com/springboot/main/EgovBootApplication.java
@@ -7,46 +7,44 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author 배치실행개발팀
  * @since 2021. 11.25
  * @version 1.0
  * @see
- *  <pre>
+ * 
+ *      <pre>
  *      개정이력(Modification Information)
  *
  *  수정일         수정자          수정내용
  *  ----------   -----------   ---------------------------
  *  2021.11.25   신용호          최초 생성
  *  2023.03.15   신용호          java Config 및 Annotation 적용
- *  
- *  </pre>
+ * 
+ *      </pre>
  */
 
 @SpringBootApplication
-@ComponentScan(basePackages="egovframework")
+@ComponentScan(basePackages = "egovframework")
 @Import(EgovBootInitialization.class)
 @EnableBatchProcessing
 public class EgovBootApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(EgovBootApplication.class, args);
+		// SpringApplication.run(EgovBootApplication.class, args);
 
 		System.out.println("##### EgovSampleBootApplication Start #####");
 
-		ApplicationContext context = new SpringApplicationBuilder(EgovBootApplication.class)
-                .headless(false)
-                .bannerMode(Banner.Mode.CONSOLE)
-                .run(args);
+		ApplicationContext context = new SpringApplicationBuilder(EgovBootApplication.class).headless(false)
+				.bannerMode(Banner.Mode.CONSOLE).run(args);
 		/*
-		SpringApplication springApplication = new SpringApplication(EgovBootApplication.class);
-		springApplication.setBannerMode(Banner.Mode.OFF);
-		springApplication.setLogStartupInfo(false);
-		springApplication.run(args);
-		*/
+		 * SpringApplication springApplication = new
+		 * SpringApplication(EgovBootApplication.class);
+		 * springApplication.setBannerMode(Banner.Mode.OFF);
+		 * springApplication.setLogStartupInfo(false); springApplication.run(args);
+		 */
 		System.out.println("##### EgovSampleBootApplication End #####");
 	}
-	
+
 }
